b2c90c8cb2fa8bc6a8e1652e97938eaa3ae37ed3
Jan Lafferton <jan@lafferton.de>
2024-06-26 20:26:15 +0200

    Working winit example

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use glfw::fail_on_errors;
+use winit::{
+    application::ApplicationHandler,
+    dpi::PhysicalSize,
+    event::{Event, WindowEvent},
+    event_loop::EventLoop,
+    window::{Window, WindowAttributes},
+};
 
 struct Graphics<'a> {
     instance: wgpu::Instance,
@@ -7,12 +13,11 @@ struct Graphics<'a> {
     device: wgpu::Device,
     queue: wgpu::Queue,
     config: wgpu::SurfaceConfiguration,
-    window_size: (i32, i32),
 }
 
 impl<'a> Graphics<'a> {
-    async fn init(window: &'a glfw::PWindow) -> Graphics<'a> {
-        let window_size = window.get_size();
+    async fn init(window: &'a Window) -> Graphics<'a> {
+        let window_size = window.inner_size();
 
         let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
             backends: wgpu::Backends::PRIMARY,
@@ -53,13 +58,15 @@ impl<'a> Graphics<'a> {
         let config = wgpu::SurfaceConfiguration {
             usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
             format: surface_format,
-            width: window_size.0 as u32,
-            height: window_size.1 as u32,
+            width: window_size.width,
+            height: window_size.height,
             present_mode: surface_caps.present_modes[0],
             alpha_mode: surface_caps.alpha_modes[0],
             view_formats: vec![],
             desired_maximum_frame_latency: 2,
         };
+
+        surface.configure(&device, &config);
 
         Self {
             instance,
@@ -68,30 +75,44 @@ impl<'a> Graphics<'a> {
             device,
             queue,
             config,
-            window_size,
         }
+    }
+
+    fn handle_resize(&mut self, size: PhysicalSize<u32>) {
+        self.config.width = size.width;
+        self.config.height = size.height;
+        self.surface.configure(&self.device, &self.config);
     }
 }
 
 async fn run() {
-    let mut glfw = glfw::init(fail_on_errors!()).unwrap();
+    let event_loop = EventLoop::new().unwrap();
 
-    let (mut window, events) = glfw
-        .create_window(800, 600, "Hello World", glfw::WindowMode::Windowed)
+    let window = event_loop
+        .create_window(WindowAttributes::default())
         .unwrap();
 
-    glfw.window_hint(glfw::WindowHint::ClientApi(glfw::ClientApiHint::NoApi));
-    window.set_key_polling(true);
+    let mut graphics = Graphics::init(&window).await;
 
-    let state = Graphics::init(&window).await;
-
-    while !window.should_close() {
-        glfw.poll_events();
-
-        for (_, event) in glfw::flush_messages(&events) {
-            // TODO
-        }
-    }
+    event_loop
+        .run(move |event, elwt| {
+            if let Event::WindowEvent {
+                window_id: _,
+                event,
+            } = event
+            {
+                match event {
+                    WindowEvent::Resized(new_size) => {
+                        graphics.handle_resize(new_size);
+                    }
+                    WindowEvent::CloseRequested => {
+                        elwt.exit();
+                    }
+                    _ => {}
+                }
+            }
+        })
+        .unwrap();
 }
 
 fn main() {
